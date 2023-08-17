2014c1484f74635e3e2f4c23122a7d32cb13926b
liuzh <liuzh@liuzh.com>
2023-08-17 22:43:47 +0800

    init

--- launcher.py
+++ launcher.py
@@ -3,6 +3,7 @@ from modules.launch_utils import git_clone, repo_dir
 
 
 def prepare_environment():
+    print(f"Git the roop to repositories")
     roop_repo = os.environ.get('ROOP_REPO', 'https://github.com/s0md3v/roop.git')
 
     roop_commit_hash = os.environ.get('ROOP_COMMIT_HASH', "1a8d37126174bb4e281acad35a258afe3a98e667")
