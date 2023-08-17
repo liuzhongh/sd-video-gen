c14d40dd230ef7e3b3de00aa420c9370378adbd3
liuzh <liuzh@liuzh.com>
2023-08-17 23:02:44 +0800

    init

--- install.py
+++ install.py
@@ -6,7 +6,7 @@ def prepare_environment():
     print(f"Git the roop to repositories")
     roop_repo = os.environ.get('ROOP_REPO', 'https://github.com/s0md3v/roop.git')
 
-    roop_commit_hash = os.environ.get('ROOP_COMMIT_HASH', "1a8d37126174bb4e281acad35a258afe3a98e667")
+    roop_commit_hash = os.environ.get('ROOP_COMMIT_HASH', "55b242c17aaec524fba04cda5508e1b4326f78c7")
 
     print(f"Roop repo: {launch.repo_dir('roop-repo')}")
     launch.git_clone(roop_repo, launch.repo_dir('roop-repo'), "Roop", roop_commit_hash)
