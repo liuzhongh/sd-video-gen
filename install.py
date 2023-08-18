e453c1317e4d481c6099b2e1972e9d57999f6622
liuzh <liuzh@liuzh.com>
2023-08-18 11:51:41 +0800

    init

--- install.py
+++ install.py
@@ -4,16 +4,12 @@ import launch
 
 
 def prepare_environment():
-    current_dir = os.getcwd()
-    folder_name = os.path.basename(current_dir)
-    print(folder_name)
-
     print(f"Git the roop to repositories")
     roop_repo = os.environ.get('ROOP_REPO', 'https://github.com/s0md3v/roop.git')
 
     roop_commit_hash = os.environ.get('ROOP_COMMIT_HASH', "55b242c17aaec524fba04cda5508e1b4326f78c7")
 
-    roop_path = os.path.join(extensions_dir, "sd-video-gen", "roop")
+    roop_path = os.path.join(extensions_dir, "sd-video-gen-roop", "roop")
     print(f"Roop repo: {roop_path}")
     launch.git_clone(roop_repo, roop_path, "Roop", roop_commit_hash)
 
