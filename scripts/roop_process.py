554d75831e762aed9fb74202302c50e914bf79f5
liuzh <liuzh@liuzh.com>
2023-08-18 23:27:38 +0800

    init

--- scripts/roop_process.py
+++ scripts/roop_process.py
@@ -169,11 +169,8 @@ def clean_temp(target_path: str) -> None:
     temp_directory_path = get_temp_directory_path(target_path)
     parent_directory_path = os.path.dirname(temp_directory_path)
     logger.info("temp_directory_path %s, parent_directory_path %s, keep_frames %s", temp_directory_path, parent_directory_path, roop.globals.keep_frames)
-    if os.path.isdir(temp_directory_path):
-        shutil.rmtree(temp_directory_path)
-    logger.info("os.listdir(parent_directory_path) %s", os.listdir(parent_directory_path))
-    if os.path.exists(parent_directory_path) and not os.listdir(parent_directory_path):
-        os.rmdir(parent_directory_path)
+    if os.path.isdir(parent_directory_path):
+        shutil.rmtree(parent_directory_path)
 
 
 def move_temp(target_path: str, output_path: str) -> None:
